d936b134b8dd33e23632bc437a0cb2c3f29339c2
Ricky dildy <rdildy92@gmail.com>
2019-12-22 17:20:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,7 @@ def oxford_comma(array=[])
     "kiwi"
        array << "kiwi"
        array =("kiwi")
-    if array.length ==1 
-      return [0]
-      elsif 
-    end 
+   
 end              
 end 
 
