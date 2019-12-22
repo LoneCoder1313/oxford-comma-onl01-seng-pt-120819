bfe66632bbac652973d79daf1bfe8685f1f2624c
Ricky dildy <rdildy92@gmail.com>
2019-12-22 15:50:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array=[])
        data= "kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos"
     array.unshift("kiwi")
       array=("kiwi") 
-      while array = ("kiwi")
+      if array 
       
       end 
           
