fe415091f2bc01dd2bd3639dde6214f830836856
Ricky dildy <rdildy92@gmail.com>
2019-12-22 17:08:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
 def oxford_comma(array=[])
-    array.unshift("kiwi")
-      array = ("kiwi")
+
       if array.length == 1 
         return array[0]
       end
              
-    end 
+end 
 
           
