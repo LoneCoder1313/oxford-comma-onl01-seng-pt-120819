c87f1b70feae54c2fb07c5d8e23255f51581feb5
Ricky dildy <rdildy92@gmail.com>
2019-12-22 16:14:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,6 @@
 def oxford_comma(array=[])
     array.unshift("kiwi")
       array=("kiwi") 
-    array.join(",")+("durian")
-    puts "kiwi", "durian"
-    array = ("kiwi", "durian")
     end 
 
           
