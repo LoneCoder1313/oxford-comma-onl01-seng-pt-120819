d001289a0e757a8b1be81ba034f18b6534b10a6f
Ricky dildy <rdildy92@gmail.com>
2019-12-22 17:01:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,7 @@
 def oxford_comma(array=[])
     array.unshift("kiwi")
       array = ("kiwi")
-      "durian"
-      array << "durian"
-      array= ("kiwidurian")
-      array.split 
+      
     end 
 
           
