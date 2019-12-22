3df8492b413dfee7ecf555d1edb5d966d617c3df
Ricky dildy <rdildy92@gmail.com>
2019-12-22 16:48:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,7 @@ def oxford_comma(array=[])
     array.unshift("kiwi")
       array=("kiwi")
       if string= "kiwi and durian"
+        ("and")== (",")
         puts array = ("kiwi", "durian")
       end 
     end 
