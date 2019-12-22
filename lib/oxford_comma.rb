e70fc53110a543c60283ef55732d572df047a060
Ricky dildy <rdildy92@gmail.com>
2019-12-22 16:07:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,10 @@ def oxford_comma(array=[])
        data= "kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos"
     array.unshift("kiwi")
       array=("kiwi") 
-      if data > array
-        array << ("durian")
+      if array.index = [0] do 
+        array << "durian"
+        then array.index = [1]
+        puts array = ["kiwi", "durian"]
     end 
     and 
       end 
