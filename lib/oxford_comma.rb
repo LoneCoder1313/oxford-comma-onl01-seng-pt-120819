00d39cedd81577c1c4daf7ab65bae5ed17c2ae43
Ricky dildy <rdildy92@gmail.com>
2019-12-22 16:14:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,4 @@
 def oxford_comma(array=[])
-       "kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos"
     array.unshift("kiwi")
       array=("kiwi") 
     array.join(",")+("durian")
