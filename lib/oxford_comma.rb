3f1cab4e8d1c6bd6bc511cac823d55f5d9a43861
Ricky dildy <rdildy92@gmail.com>
2019-12-22 16:36:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array=[])
     array.unshift("kiwi")
       array=("kiwi") 
-      array.to_string 
+      array.to_str 
       string = "kiwi"
       string << ("durian").split(",")
       string = "kiwi, durian"
